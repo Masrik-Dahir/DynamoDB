999de3b418471428b961bb4953e0068acb2ae4a1
masri <dahirma@vcu.edu>
2022-05-16 01:13:37 -0400

    done

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ AWS.config.update ({region: "us-east-1"});
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 // // 1. Create an organization
+// const happyIncOrgId = "c89608cf-39a7-4589-9eea-173ae87192da";
 // const orgId = uuidv4();
 
 // var params = {
 //     TableName : 'happy-projects',
 //     Item: {
-//        PK: `ORG#${orgId}`,
+//        PK: `ORG#${happyIncOrgId}`,
 //        SK: `#METADATA#${orgId}`,
-//        name: "Happy Inc",
+//        name: "Made to Delete",
 //        tier: "professional"
 //     }
 //   };
@@ -55,29 +56,57 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 //     }
 //   };
   
-//   var documentClient = new AWS.DynamoDB.DocumentClient();
-  
 //   dynamodb.update(params, function(err, data) {
 //      if (err) console.log(err);
 //      else console.log(data);
 //   });
 
 
-// 4. Get an organizaiton
-const happyIncOrgId = "c89608cf-39a7-4589-9eea-173ae87192da";
-var params = {
-    TableName: 'happy-projects',
-    Key: { PK : `ORG#${happyIncOrgId}`, SK: `#METADATA#${happyIncOrgId}`},
-    UpdateExpression: 'set #name = :name',
-    ExpressionAttributeNames: {'#name' : 'name'},
-    ExpressionAttributeValues: {
-      ':name' : "Updated",
-    }
-  };
+// // 4. Get an organizaiton
+// const happyIncOrgId = "c89608cf-39a7-4589-9eea-173ae87192da";
+// var params = {
+//     TableName : 'happy-projects',
+//     Key: {
+//         PK : `ORG#${happyIncOrgId}`, 
+//         SK: `#METADATA#${happyIncOrgId}`
+//     }
+//   };
+    
+//   dynamodb.get(params, function(err, data) {
+//     if (err) console.log(err);
+//     else console.log(data);
+//   });
+
+
+// // 4. Get all organizaiton
+// const happyIncOrgId = "c89608cf-39a7-4589-9eea-173ae87192da";
+// var params = {
+//     TableName: 'happy-projects',
+//     KeyConditionExpression: '#PK = :PK and begins_with(#SK, :SK)',
+//     ExpressionAttributeNames: {'#PK': 'PK', '#SK' : 'SK'},
+//     ExpressionAttributeValues: {
+//       ':PK': `ORG#${happyIncOrgId}`,
+//       ':SK': `#METADATA#`
+//     }
+//   };
+    
+//   dynamodb.query(params, function(err, data) {
+//      if (err) console.log(err);
+//      else console.log(data);
+//   });
+
+// // Delete
+// const PK_Id = "c89608cf-39a7-4589-9eea-173ae87192da";
+// const SK_Id = "29853cbe-4e5b-418a-b0fe-e60b0654ec1d";
+// var params = {
+//     TableName : 'happy-projects',
+//     Key: {
+//         PK : `ORG#${PK_Id}`, 
+//         SK: `#METADATA#${SK_Id}`
+//     }
+//   };
   
-  var documentClient = new AWS.DynamoDB.DocumentClient();
-  
-  dynamodb.update(params, function(err, data) {
-     if (err) console.log(err);
-     else console.log(data);
-  });
+//   dynamodb.delete(params, function(err, data) {
+//     if (err) console.log(err);
+//     else console.log(data);
+//   });
